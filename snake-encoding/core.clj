8dc4bcf47527f977e2ef71a6b04b4e1a9ff09fcb
Jeffrey Simon <jdsimon@gmail.com>
2016-07-01 09:33:29 -0400

    way cleaner

--- snake-encoding/core.clj
+++ snake-encoding/core.clj
@@ -1,49 +1,20 @@
 (ns snake-encoding.core
   (:require [clojure.string :as str])
-  (:import (java.io BufferedReader))
   (:gen-class))
 
-(defn circular-shift-right [n coll]
-  (concat (take-last n coll) (drop-last n coll)))
-
-(defn column [table c]
-  (mapv #(get % c) table))
-
-(defn table->snake-seq [table]
-  (flatten (reduce (fn [s i]
-                     (if (even? i)
-                       (conj s (reverse (column table i)))
-                       (conj s (column table i))))
-                   []
-                   (range (count table)))))
-
-(defn snake-seq->table [snake-seq]
-  (let [n (int (Math/sqrt (count snake-seq)))
-        partitioned (map-indexed (fn [i p]
-                                   (vec (if (even? i)
-                                          (reverse p)
-                                          p)))
-                                 (partition n snake-seq))]
-    (reduce (fn [table i]
-              (conj table (mapv #(get % i) partitioned)))
-            []
-            (range n))))
-
-(defn lines->snake [lines]
-  (reduce (fn [v line]
-            (if (not (str/blank? line))
-              (conj v (vec line))
-              v))
-          []
-          lines))
-
 (defn -main [& args]
-  (let [n (read)
-        x (read)
-        snake (lines->snake (line-seq (BufferedReader. *in*)))]
-    (doseq [ls (->> snake
-                    table->snake-seq
-                    (circular-shift-right x)
-                    snake-seq->table)]
-      (println (str/join ls)))))
-
+  (let [size (read)
+        i (read)
+        _ (read-line)
+        n (* size size)]
+    (->> (repeatedly size read-line)
+         (apply map vector)
+         (mapcat #(%1 %2) (cycle [reverse identity]))
+         cycle
+         (drop (- n i))
+         (take n)
+         (partition size)
+         (map #(%1 %2) (cycle [reverse identity]))
+         (apply map str)
+         (str/join \newline)
+         println)))
