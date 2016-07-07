aa6318cb7e187eceb07c8723b711a122b72b5ace
Jeffrey Simon <jdsimon@gmail.com>
2016-07-06 21:39:11 -0400

    adding the ideal solution

---
+++ anagrams/ideal.clj
@@ -0,0 +1,19 @@
+(ns Solution
+  (:gen-class))
+
+(defn nth-letter? [n ch]
+  (contains? (set (take-nth n "$ABCDEFGHIJKLMNOPQRSTUVWXYZ")) ch))
+
+(defn scramble [s pred f]
+  (let [A (filter (comp pred second) (map-indexed vector s))
+        A (map vector (map first A) (f (map second A)))]
+    (apply str (reduce #(apply assoc %1 %2) (vec s) A))))
+
+(defn -main [& args]
+  (-> (read-line)
+      (reverse)
+      (scramble #(not= \space %) reverse)
+      (scramble #(nth-letter? 4 %) #(concat (take-last 1 %) (drop-last 1 %)))
+      (scramble #(nth-letter? 3 %) #(concat (drop 1 %) (take 1 %)))
+      (scramble #(nth-letter? 2 %) reverse)
+      (println)))
